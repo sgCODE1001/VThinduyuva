0d164a7708f9d70a56cc5c0f8e3837b3d5651f47
sgCODE1001 <shreelvt2028@gmail.com>
2025-05-03 00:16:14 -0400

    Initial commit

---
+++ app.py
@@ -0,0 +1,27 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+# @app.route("/about")
+# def about():
+#     return render_template("about.html")
+
+@app.route("/members")
+def members():
+    return render_template("members.html")
+
+# @app.route("/events")
+# def events():
+#     return render_template("events.html")
+
+@app.route("/donate")
+@app.route("/tickets")
+def donate_and_tickets():
+    return render_template("donate.html")
+
+if __name__ == "__main__":
+    app.run(debug=True)
